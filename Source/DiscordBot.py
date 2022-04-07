01346ae600fbd91be513a3813cc36ef195e83e2b
Tom Burniston <tomburniston@gmail.com>
2022-04-07 07:10:17 -0700

    Added User Checking and Bot can access files
    Bot can now check for certain users and sent personalized messages about them. Bot can also access and change files.

--- Source/DiscordBot.py
+++ Source/DiscordBot.py
@@ -3,12 +3,16 @@ import os
 
 client = discord.Client()
 
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+    
+    
     if message.author == client.user:
         return
     if message.content.startswith('$hello'):
@@ -21,8 +25,20 @@ async def on_message(message):
 
     if ' tom ' in message.content:
       await message.channel.send('you called?')
-
+    if message.author.id == 290327583843942400:
       
+      f = open("messageStats.txt", "r")
+      oldNum = int(f.read())
+      print(oldNum)
+      num = oldNum +1
+      f.close()
+      f = open("messageStats.txt", "w")
+      f.write(f"{num}")
+      f.close()
+      await message.channel.send(f"The number of annoying messages from Jake is {num}")
+
+    
     
 
-client.run('NULL')
+client.run('OTYxNDYyMTk4OTc3MzIzMDYw.Yk5Vcw.YJjPwSS6feEQ99L_jAevVDF3o5M')
+numOfTomMessages = 0
